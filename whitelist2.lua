00e2116a0267e4e43eec6bb6eab86093df5b5761
Devin <skibidihubhellyea@gmail.com>
2024-12-08 21:51:27 +0700

    Added Mellokingthe3rd to the whitelist

--- whitelist2.lua
+++ whitelist2.lua
@@ -1,3 +1,3 @@
-local whitelisted = {"HASUAWUH", "Devinja89", "roblox", "nicekid0797", "lit6j1", "bjarnos", "lwbnl"}
+local whitelisted = {"HASUAWUH", "Devinja89", "roblox", "nicekid0797", "lit6j1", "bjarnos", "lwbnl", "Mellokingthe3rd"}
 
 return whitelisted
