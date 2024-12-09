e568317d6d5ed58fcdd8596e837ab9be152aa7e0
Devin <skibidihubhellyea@gmail.com>
2024-12-10 06:04:37 +0700

    Added THEJITU to the whitelist

--- whitelist2.lua
+++ whitelist2.lua
@@ -1,3 +1,3 @@
-local whitelisted = {"HASUAWUH", "Devinja89", "roblox", "nicekid0797", "lit6j1", "bjarnos", "lwbnl", "Mellokingthe3rd", "USARMYINSTRUCTERRR", "MINECARFTVSJ2TTTTTT", "OfflciaIGojo_TSB"}
+local whitelisted = {"HASUAWUH", "Devinja89", "roblox", "nicekid0797", "lit6j1", "bjarnos", "lwbnl", "Mellokingthe3rd", "USARMYINSTRUCTERRR", "MINECARFTVSJ2TTTTTT", "OfflciaIGojo_TSB", "THEJITU"}
 
 return whitelisted
