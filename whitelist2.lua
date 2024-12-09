8b24048b93193a8b5aad950f253f9362c847280a
Devin <skibidihubhellyea@gmail.com>
2024-12-09 13:23:25 +0700

    Added MINECARFTVSJ2TTTTTT to the whitelist

--- whitelist2.lua
+++ whitelist2.lua
@@ -1,3 +1,3 @@
-local whitelisted = {"HASUAWUH", "Devinja89", "roblox", "nicekid0797", "lit6j1", "bjarnos", "lwbnl", "Mellokingthe3rd", "USARMYINSTRUCTERRR"}
+local whitelisted = {"HASUAWUH", "Devinja89", "roblox", "nicekid0797", "lit6j1", "bjarnos", "lwbnl", "Mellokingthe3rd", "USARMYINSTRUCTERRR", "MINECARFTVSJ2TTTTTT"}
 
 return whitelisted
