a214872e1ec4663dc8f7d9673bca7656da7cbf7a
Devin <skibidihubhellyea@gmail.com>
2024-12-13 03:10:50 +0700

    Added emms2 to the whitelist

--- whitelist2.lua
+++ whitelist2.lua
@@ -1,3 +1,3 @@
-local whitelisted = {"HASUAWUH", "Devinja89", "roblox", "nicekid0797", "lit6j1", "bjarnos", "lwbnl", "Mellokingthe3rd", "USARMYINSTRUCTERRR", "MINECARFTVSJ2TTTTTT", "OfflciaIGojo_TSB", "THEJITU", "<ppppur233>", "justindanielpa1", "ppppur233", "idk_s3.", "hagsfa19"}
+local whitelisted = {"HASUAWUH", "Devinja89", "roblox", "nicekid0797", "lit6j1", "bjarnos", "lwbnl", "Mellokingthe3rd", "USARMYINSTRUCTERRR", "MINECARFTVSJ2TTTTTT", "OfflciaIGojo_TSB", "THEJITU", "<ppppur233>", "justindanielpa1", "ppppur233", "idk_s3.", "hagsfa19", "emms2"}
 
 return whitelisted
