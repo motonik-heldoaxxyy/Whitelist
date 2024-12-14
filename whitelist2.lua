98d23c89b881cce2f642b1909b2d3444da20b5e4
Devin <skibidihubhellyea@gmail.com>
2024-12-14 22:49:06 +0700

    Added nong_porsche9 to the whitelist

--- whitelist2.lua
+++ whitelist2.lua
@@ -1,3 +1,3 @@
-local whitelisted = {"HASUAWUH", "Devinja89", "roblox", "nicekid0797", "lit6j1", "bjarnos", "lwbnl", "Mellokingthe3rd", "USARMYINSTRUCTERRR", "MINECARFTVSJ2TTTTTT", "OfflciaIGojo_TSB", "THEJITU", "ppppur233", "justindanielpa11", "ppppur233", "idk_s3.", "hagsfa19", "emms2", "dvd2019ok", "RED_DRAGON90224", "edsheeran12035", "<@936225802893275157>"}
+local whitelisted = {"HASUAWUH", "Devinja89", "roblox", "nicekid0797", "lit6j1", "bjarnos", "lwbnl", "Mellokingthe3rd", "USARMYINSTRUCTERRR", "MINECARFTVSJ2TTTTTT", "OfflciaIGojo_TSB", "THEJITU", "ppppur233", "justindanielpa11", "ppppur233", "idk_s3.", "hagsfa19", "emms2", "dvd2019ok", "RED_DRAGON90224", "edsheeran12035", "<@936225802893275157>", "nong_porsche9"}
 
 return whitelisted
