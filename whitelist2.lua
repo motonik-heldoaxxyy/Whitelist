cc14293025b487cb6c852dd3401a4d3322031de1
Devin <skibidihubhellyea@gmail.com>
2025-01-28 02:49:12 +0700

    wockvist is now whitelisted!

--- whitelist2.lua
+++ whitelist2.lua
@@ -1,3 +1,3 @@
-local whitelisted = {"HASUAWUH", "Devinja89", "Bayeyou45, "bigbootygyat", "wockvist", "kyslore", "wockvist"}
+local whitelisted = {"HASUAWUH", "Devinja89", "Bayeyou45, "bigbootygyat", "wockvist", "kyslore", "wockvist", "wockvist"}
 
 return whitelisted
